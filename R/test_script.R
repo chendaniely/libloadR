be561ce78ae975eff2f9e9cd814fa7dc8d91fa63
Daniel Chen <Maveri2k@yahoo.com>
2015-05-28 00:44:33 -0400

    comment out lines from test_script

--- R/test_script.R
+++ R/test_script.R
@@ -1,33 +1,33 @@
-# some
-
-# random
-
-#lines
-
-##### of
-
-# code
-
-3 + 4
-
-var <- 'something'
-
-remove.packages("gapminder")
-detach("package:gapminder", unload=TRUE)
-
-library(gapminder)
-require(gapminder)
-library(ggplot2)
-
-rm(list = ls())
-source(file = 'R/parse_r_script.R')
-
-libload('R/test_script.R')
-
-# libraries <- parse_r_script('R/test_script.R')
-# pkgs <- sapply(X = libraries, FUN = parse_load)
+# # some
 #
-# sapply(X = pkgs, FUN = load_and_or_install)
+# # random
 #
-# var <- "dplyr"
-# library(var, character.only = TRUE)
+# #lines
+#
+# ##### of
+#
+# # code
+#
+# 3 + 4
+#
+# var <- 'something'
+#
+# remove.packages("gapminder")
+# detach("package:gapminder", unload=TRUE)
+#
+# library(gapminder)
+# require(gapminder)
+# library(ggplot2)
+#
+# rm(list = ls())
+# source(file = 'R/parse_r_script.R')
+#
+# libload('R/test_script.R')
+#
+# # libraries <- parse_r_script('R/test_script.R')
+# # pkgs <- sapply(X = libraries, FUN = parse_load)
+# #
+# # sapply(X = pkgs, FUN = load_and_or_install)
+# #
+# # var <- "dplyr"
+# # library(var, character.only = TRUE)
