0a0d73ddc94011fc6803667c1049ad9d23fed964
Daniel Chen <Maveri2k@yahoo.com>
2015-05-28 12:41:39 -0400

    documentation for functions

--- R/libload.R
+++ R/libload.R
@@ -1,4 +1,11 @@
 #' Parses a file and returns the lines that load a library
+#'
+#' This function takes a file and will return the lines of the file that
+#' match the given library_pattern
+#'
+#' @param file R script file
+#' @param library_pattern regular expression to look for lines that load packages
+#' @return library_lines lines of the file that load a package
 .parse_r_script <- function(file,
                            library_pattern = '^\\s*(library|require)\\(\\S*?\\)'){
     lines <- readLines(file)
@@ -9,6 +16,12 @@
 }
 
 #' Parses the name of the library to be loaded
+#'
+#' This function takes in the load commands from .parse_r_script and only
+#' returns the package name
+#'
+#' @param load_command line of the file that loads a package
+#' @return library_name returns the library name that was in the load_command
 .parse_load <- function(load_command){
     open_paren <- regexpr(pattern = '\\(', text = load_command)[1]
     close_paren <- regexpr(pattern = '\\)', text = load_command)[1]
@@ -16,6 +29,11 @@
 }
 
 #' Load library if it exists, otherwise install the package
+#'
+#' This function will try to load a package name, if the package does not exist
+#' it will install the package and then load it
+#'
+#' @param package_name package name to load
 .load_and_or_install <- function(package_name){
     if (!require(package_name, character.only = TRUE)) {
         install.packages(package_name, dependencies = TRUE)
@@ -23,6 +41,13 @@
     library(package_name, character.only = TRUE)
 }
 
+#' Load all libraries from an R script and install them as needed
+#'
+#' This function will take a R script, load any packages, and install packages
+#' as needed
+#'
+#' @param file R script file to load
+#' @param ... library_pattern to be passed into .parse_r_script
 libload <- function(file, ...){
     libraries <- .parse_r_script(file, ...)
     pkgs <- sapply(X = libraries, FUN = .parse_load)
