e675ad6a51c55e09cd3d1cc6c6dae4b15420d86c
Daniel Chen <Maveri2k@yahoo.com>
2015-05-28 12:02:03 -0400

    hide helper functions

--- R/parse_r_script.R
+++ R/parse_r_script.R
@@ -1,5 +1,5 @@
 #' Parses a file and returns the lines that load a library
-parse_r_script <- function(file,
+.parse_r_script <- function(file,
                            library_pattern = '^\\s*(library|require)\\(\\S*?\\)'){
     lines <- readLines(file)
     # print(lines)
@@ -9,14 +9,14 @@ parse_r_script <- function(file,
 }
 
 #' Parses the name of the library to be loaded
-parse_load <- function(load_command){
+.parse_load <- function(load_command){
     open_paren <- regexpr(pattern = '\\(', text = load_command)[1]
     close_paren <- regexpr(pattern = '\\)', text = load_command)[1]
     substr(x = load_command, start = open_paren + 1, stop = close_paren - 1)
 }
 
 #' Load library if it exists, otherwise install the package
-load_and_or_install <- function(package_name){
+.load_and_or_install <- function(package_name){
     if (!require(package_name, character.only = TRUE)) {
         install.packages(package_name, dependencies = TRUE)
     }
@@ -24,7 +24,7 @@ load_and_or_install <- function(package_name){
 }
 
 libload <- function(file, ...){
-    libraries <- parse_r_script(file, ...)
-    pkgs <- sapply(X = libraries, FUN = parse_load)
-    sapply(X = pkgs, FUN = load_and_or_install)
+    libraries <- .parse_r_script(file, ...)
+    pkgs <- sapply(X = libraries, FUN = .parse_load)
+    sapply(X = pkgs, FUN = .load_and_or_install)
 }
