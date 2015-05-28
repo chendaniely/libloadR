064740357ad32221a4c8447ac36cfe5d2b21d2fc
Daniel Chen <Maveri2k@yahoo.com>
2015-05-28 12:46:30 -0400

    example is just syntax, do not execute

--- R/libload.R
+++ R/libload.R
@@ -51,7 +51,7 @@
 #'
 #' @export
 #'
-#' @example libload('r_script_file.R')
+#' @example # libload('r_script_file.R')
 libload <- function(file, ...){
     libraries <- .parse_r_script(file, ...)
     pkgs <- sapply(X = libraries, FUN = .parse_load)
